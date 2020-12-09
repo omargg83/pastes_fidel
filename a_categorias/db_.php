5366bd582d7339fcca4c43b227ef926af138c93e
ericesponda <ericesponda@yahoo.com.mx>
2020-12-09 16:15:09 -0600

    x
    x

--- a_categorias/db_.php
+++ a_categorias/db_.php
@@ -16,9 +16,9 @@ class Categoria extends Sagyc{
 	public $nivel_captura;
 	public function __construct(){
 		parent::__construct();
-		if(isset($_SESSION['idusuario']) and $_SESSION['autoriza'] == 1 and array_key_exists('CATEGORIA', $this->derecho)) {
+		if(isset($_SESSION['idusuario']) and $_SESSION['autoriza'] == 1 and array_key_exists('INVENTARIO', $this->derecho)) {
 			////////////////PERMISOS
-			$sql="SELECT nivel,captura FROM usuarios_permiso where idusuario='".$_SESSION['idusuario']."' and modulo='CATEGORIA'";
+			$sql="SELECT nivel,captura FROM usuarios_permiso where idusuario='".$_SESSION['idusuario']."' and modulo='INVENTARIO'";
 			$stmt= $this->dbh->query($sql);
 
 			$row =$stmt->fetchObject();
