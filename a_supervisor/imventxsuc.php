2085f5b743b4be254bf6175243e928ee49073981
ericesponda <>
2020-12-14 02:00:39 -0600

    x
    x

--- a_supervisor/imventxsuc.php
+++ a_supervisor/imventxsuc.php
@@ -9,13 +9,9 @@
 	$pdf->selectFont('Helvetica');
 	// la imagen solo aparecera si antes del codigo ezStream se pone ob_end_clean como se muestra al final men
 
-
-	$idusuario=$_REQUEST['idusuario'];
 	$idsucursal=$_REQUEST['idsucursal'];
 	$fechayhora=new DateTime();
 
-	$desde = date("Y-m-d", strtotime($desde))." 00:00:00";
-	$hasta = date("Y-m-d", strtotime($hasta))." 23:59:59";
 	$sql="SELECT
 	productos_catalogo.nombre,
 	productos_catalogo.codigo,
